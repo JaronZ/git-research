77b6b26b695c2a6a3375c47bdf70273fce54a52f
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2024-11-29 20:54:19 +0100

    feat: add timers for different sorting algorithms

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const unsorted = require("../items.json");
+
+console.time("Insertion Sort");
+console.timeEnd("Insertion Sort");
+
+console.time("Merge Sort");
+console.timeEnd("Merge Sort");
+
+console.time("Quick Sort");
+console.timeEnd("Quick Sort");
+
